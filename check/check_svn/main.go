717e95b8610e46d71ee69aaf726849b985d3cb29
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-08-31 15:54:19 +0200

    Defers are not executed on exits, so convert to returns

--- check/check_svn/main.go
+++ check/check_svn/main.go
@@ -11,10 +11,10 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
-func critical(msg string, err error) {
+func critical(msg string, err error) int {
 	fmt.Printf("CRITICAL | %s: %s\n", msg, err)
 
-	os.Exit(2)
+	return 2
 }
 
 var opts struct {
@@ -46,51 +46,55 @@ func runToStd(cmd ...string) (exitStatus int, err error) {
 	return 0, nil
 }
 
-func main() {
+func cmd() (exitStatus int) {
 	_, err := flags.Parse(&opts)
 	if err != nil {
-		critical("parsing arguments", err)
+		return critical("parsing arguments", err)
 	}
 
 	tmp, err := ioutil.TempDir("", "check_svn")
 	if err != nil {
-		critical("create temporary directory", err)
+		return critical("create temporary directory", err)
 	}
 	defer func() {
 		err = os.RemoveAll(tmp)
 		if err != nil {
-			critical("remove temporary directory", err)
+			exitStatus = critical("remove temporary directory", err)
 		}
 	}()
 
 	_, err = runToStd("svn", "checkout", "--username", opts.User, "--password", opts.Password, opts.Repository, tmp)
 	if err != nil {
-		critical("checkout repository", err)
+		return critical("checkout repository", err)
 	}
 
 	err = os.Chdir(tmp)
 	if err != nil {
-		critical("change to temporary directory", err)
+		return critical("change to temporary directory", err)
 	}
 
 	now := fmt.Sprintf("%d", time.Now().Unix())
 
 	err = ioutil.WriteFile(tmp+"/test.txt", []byte(now), 0700)
 	if err != nil {
-		critical("write to test file", err)
+		return critical("write to test file", err)
 	}
 
 	_, err = runToStd("svn", "commit", "--username", opts.User, "--password", opts.Password, "--message", now)
 	if err != nil {
-		critical("commit change", err)
+		return critical("commit change", err)
 	}
 
 	_, err = runToStd("svn", "up", "--username", opts.User, "--password", opts.Password)
 	if err != nil {
-		critical("update repository", err)
+		return critical("update repository", err)
 	}
 
 	fmt.Printf("OK - can read from and write to repository %s\n", opts.Repository)
 
-	os.Exit(0)
+	return 0
+}
+
+func main() {
+	os.Exit(cmd())
 }
